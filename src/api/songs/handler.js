84d055911f3d6e53966d1b7d1382f542f584aa00
M Akbar Maulana <makbarm.id@gmail.com>
2023-06-12 16:37:51 +0700

    add song feature

---
+++ src/api/songs/handler.js
@@ -0,0 +1,26 @@
+const autoBind = require('auto-bind');
+
+class SongsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postSongHandler(request, h) {
+    this._validator.validateSongPayload(request.payload);
+
+    const songId = await this._service.addSong(request.payload);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Song added successfully',
+      data: { songId },
+    }).code(201);
+
+    return response;
+  }
+}
+
+module.exports = SongsHandler;

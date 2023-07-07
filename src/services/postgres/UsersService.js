ee0b0f9693a59aed22dcb97e4c26873a5683dfc7
M Akbar Maulana <makbarm.id@gmail.com>
2023-07-07 17:49:19 +0700

    refactoring users

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,5 +1,5 @@
-const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
@@ -15,6 +15,7 @@ class UsersService {
 
     const id = `user-${nanoid(16)}`;
     const hashedPassword = await bcrypt.hash(password, 10);
+
     const query = {
       text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
       values: [id, username, hashedPassword, fullname],
@@ -22,23 +23,11 @@ class UsersService {
 
     const result = await this._pool.query(query);
 
-    if (!result.rowCount) {
-      throw new InvariantError('Failed to add user');
+    if (!result.rows[0].id) {
+      throw new InvariantError('Failed to register user. Missing required fields.');
     }
+
     return result.rows[0].id;
-  }
-
-  async verifyNewUsername(username) {
-    const query = {
-      text: 'SELECT username FROM users WHERE username = $1',
-      values: [username],
-    };
-
-    const result = await this._pool.query(query);
-
-    if (result.rowCount > 0) {
-      throw new InvariantError('Failed to add user. username has been used');
-    }
   }
 
   async getUserById(userId) {
@@ -56,6 +45,19 @@ class UsersService {
     return result.rows[0];
   }
 
+  async verifyNewUsername(username) {
+    const query = {
+      text: 'SELECT username FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rowCount > 0) {
+      throw new InvariantError(`Failed to add user. Username ${username} has been used.`);
+    }
+  }
+
   async verifyUserCredential(username, password) {
     const query = {
       text: 'SELECT id, password FROM users WHERE username = $1',
@@ -65,14 +67,14 @@ class UsersService {
     const result = await this._pool.query(query);
 
     if (!result.rowCount) {
-      throw new AuthenticationError('The credentials you provided are incorrect');
+      throw new AuthenticationError('The credentials you provided are incorrect.');
     }
 
     const { id, password: hashedPassword } = result.rows[0];
     const match = await bcrypt.compare(password, hashedPassword);
 
     if (!match) {
-      throw new AuthenticationError('The credentials you provided are incorrect');
+      throw new AuthenticationError('The credentials you provided are incorrect.');
     }
 
     return id;
